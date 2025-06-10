e596935ffb0cff4277aacd3db9af1414c3962f98
sparisi <sparisi@users.noreply.github.com>
2025-06-10 17:22:36 -0600

    dtype fix

--- gym_minatar/__init__.py
+++ gym_minatar/__init__.py
@@ -2,24 +2,24 @@ from gymnasium.envs.registration import register
 
 register(
     id="Gym-MinAtar/Breakout-v1",
-        entry_point="gym_minatar.breakout:Breakout",
+    entry_point="gym_minatar.breakout:Breakout",
     max_episode_steps=10_000,
 )
 
 register(
     id="Gym-MinAtar/Freeway-v1",
-        entry_point="gym_minatar.freeway:Freeway",
+    entry_point="gym_minatar.freeway:Freeway",
     max_episode_steps=10_000,
 )
 
 register(
     id="Gym-MinAtar/Asterix-v1",
-        entry_point="gym_minatar.asterix:Asterix",
+    entry_point="gym_minatar.asterix:Asterix",
     max_episode_steps=10_000,
 )
 
 register(
     id="Gym-MinAtar/Seaquest-v1",
-        entry_point="gym_minatar.seaquest:Seaquest",
+    entry_point="gym_minatar.seaquest:Seaquest",
     max_episode_steps=10_000,
 )
--- gym_minatar/freeway.py
+++ gym_minatar/freeway.py
@@ -87,7 +87,7 @@ class Freeway(gym.Env):
             raise ValueError("illegal action")
 
     def get_state(self):
-        board = np.zeros((self.n_rows, self.n_cols, 2))
+        board = np.zeros((self.n_rows, self.n_cols, 2), dtype=np.int64)
         board[self.chicken_row, self.chicken_col, 0] = 1
         for car in self.cars:
             row, col, speed, dir, timer = car
