bba0a1838708b478b97661af2103b284d3729ece
sparisi <sparisi@users.noreply.github.com>
2025-06-22 15:14:35 -0600

    docs + move only up/down

--- gym_minatar/freeway.py
+++ gym_minatar/freeway.py
@@ -5,10 +5,8 @@ from typing import Optional
 
 # Action IDs
 NOP = 0
-LEFT = 1
-RIGHT = 2
-UP = 3
-DOWN = 4
+UP = 1
+DOWN = 2
 
 RED = (255, 0, 0)  # cars
 PALE_RED = (255, 155, 155)  # car trails
@@ -20,17 +18,16 @@ class Freeway(gym.Env):
     """
     The player (chicken) starts at the bottom of the screen and must reach the top,
     crossing lanes of moving cars.
-    - The player can move up or down, but only every few frames (player speed).
-    - Cars move horizontally at different speeds and directions, wrapping around
-      the screen.
-    - If the player is hit by a car, it is sent back to the bottom row.
-    - Each time the player reaches the top, a reward of +1 is given and car speeds are randomized.
-    - The game ends after a fixed number of frames (time_limit).
-    - The observation space is a 10x10x7 boolean array:
-        - Channel 0: player (chicken) position.
-        - Channel 1: car positions.
-        - Channels 2-6: car trails, indicating car speed (from fastest to slowest).
-    - The difficulty does not ramp during an episode.
+    - The player can move up/down or may not move at all.
+    - Cars move horizontally at different (random) speeds and directions,
+      wrapping around the screen.
+    - The game ends when the player is hit by a car.
+    - Each time the player reaches the top, a reward of +1 is given and the car
+      speeds increase (still random, though).
+    - The observation space is a 3-channel grid with 0s for empty tiles, and 1 or -1
+      for information about the game entities:
+        - Channel 0: player position (1).
+        - Channel 1: car positions and their trails (-1 moving left, 1 moving right).
     """
 
     metadata = {
@@ -59,13 +56,11 @@ class Freeway(gym.Env):
         self.observation_space = gym.spaces.Box(
             -1, 1, (self.n_rows, self.n_cols, 2), dtype=np.int64,
         )
-        self.action_space = gym.spaces.Discrete(5)
+        self.action_space = gym.spaces.Discrete(3)
         self.action_map = {
             "nop": 0,
-            "left": 1,
-            "right": 2,
-            "down": 3,
-            "up": 4,
+            "up": 1,
+            "down": 2,
         }
 
         self.render_mode = render_mode
@@ -101,14 +96,14 @@ class Freeway(gym.Env):
         return self.get_state(), {}
 
     def move(self, a):
-        if a == LEFT:
-            self.player_col = max(self.player_col - 1, 0)
-        elif a == DOWN:
+        if a == DOWN:
             self.player_row = min(self.player_row + 1, self.n_rows - 1)
-        elif a == RIGHT:
-            self.player_col = min(self.player_col + 1, self.n_cols - 1)
         elif a == UP:
             self.player_row = max(self.player_row - 1, 0)
+        # elif a == RIGHT:
+        #     self.player_col = min(self.player_col + 1, self.n_cols - 1)
+        # elif a == LEFT:
+            # self.player_col = max(self.player_col - 1, 0)
         elif a == NOP:
             pass
         else:
@@ -137,11 +132,11 @@ class Freeway(gym.Env):
         return state
 
     def collision(self, row, col, action):
-        # Must check horizontal movement, otherwise the player may "step over"
+        # [COMMENTED OUT] Must check horizontal movement, otherwise the player may "step over"
         # an entity and collision won't be detected
         return (
-            ([row, col] == [self.player_row, self.player_col]) or
-            (action in [LEFT, RIGHT] and ([row, col] == [self.player_row_old, self.player_col_old]))
+            ([row, col] == [self.player_row, self.player_col]) # or
+            # (action in [LEFT, RIGHT] and ([row, col] == [self.player_row_old, self.player_col_old]))
         )
 
     def step(self, action: int):
